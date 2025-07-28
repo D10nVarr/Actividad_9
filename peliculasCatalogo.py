eeb5203c7b0a659787d97945604053dafb202384
diegobarr <d1390xd.v@gmail.com>
2025-07-27 23:08:45 -0600

    Codigo terminado con opcion 5 parcialmente completa

--- peliculasCatalogo.py
+++ peliculasCatalogo.py
@@ -38,14 +38,17 @@ def eliminar_peli(title):
 def ver_stats():
     print(f"Existen {len(peliculas)} peliculas registradas")
 
+    print("Existen estos generos de películas")
     for i in peliculas:
-        for j in i[2]:
-            print(j)
+        print(i[2])
 
-    antigua=peliculas[0][1]
+    est_antigua=peliculas[0][1]
+    antigua=peliculas[0]
+
     for i in peliculas:
-        if i[1]<antigua:
-            antigua=i[1]
+        if i[1]<est_antigua:
+            est_antigua=i[1]
+            antigua=i
     print("La pelicula más antigua es: ",antigua)
 
 while True:
@@ -66,21 +69,40 @@ while True:
             print("Pelicula agregada exitosamente")
 
         case "2":
-            print("\nLas películas ingresadas son:")
-            mostrar_peliculas()
+            if len(peliculas)>0:
+                print("\nLas películas ingresadas son:")
+                mostrar_peliculas()
+            else:
+                print("No hay peliculas registradas")
 
         case "3":
-            genero=input("\nIngrese el genero de las peliculas que desee ver: ")
-            print(f"Las peliculas catalogadas con el genero {genero} son:")
-            buscar_genero(genero)
+            if len(peliculas) > 0:
+                genero=input("\nIngrese el genero de las peliculas que desee ver: ").lower()
+                print(f"Las peliculas catalogadas con el genero {genero} son:")
+                buscar_genero(genero)
+            else:
+                print("No hay peliculas registradas")
 
         case "4":
-            titl=input("\nIngrese el titulo de la pelicula que desee eliminar: ")
-            eliminar_peli(titl)
-            print("Registro eliminado exitosamente")
+            if len(peliculas) > 0:
+                titl=input("\nIngrese el titulo de la pelicula que desee eliminar: ").lower()
+                eliminar_peli(titl)
+                print("Registro eliminado exitosamente")
+            else:
+                print("No hay peliculas registradas")
 
         case "5":
-            ver_stats()
+            if len(peliculas) > 0:
+                ver_stats()
+            else:
+                print("No hay peliculas registradas")
+
+        case "6":
+            print("\nSaliendo del programa...")
+            break
+
+        case _:
+           print("Opcion no valida")
 
 
 
