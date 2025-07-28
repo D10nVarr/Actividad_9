04a06a2b0a9410899b3143b4617913060f05b5ea
diegobarr <d1390xd.v@gmail.com>
2025-07-27 20:58:54 -0600

    Función de almacenado de peliculas por listas anidadas

--- peliculasCatalogo.py
+++ peliculasCatalogo.py
@@ -1,3 +1,43 @@
-peliculas=[]
+peliculas = []
 
-num_pls=int(input("Ingrese la cantidad de peliculas de desea registrar: "))
+def agregar_pelicula(num_pelis):
+    for i in range(num_pelis):
+        pelis=[]
+
+        titulo=input("Ingrese el título de la película: ").lower()
+        pelis.append(titulo)
+        estreno=int(input("Ingrese el año de estreno de la película: "))
+        pelis.append(estreno)
+        genero=input("Ingrese el género de la película: ").lower()
+        pelis.append(genero)
+
+        peliculas.append(pelis)
+
+def mostrar_peliculas():
+    contador=1
+    for i in peliculas:
+        print(f"Película {contador} ingresada: {i}")
+        contador+=1
+
+while True:
+    print("\n____MENÚ DE REGISTRO DE PELÍCULAS____")
+    print("1. Agregar datos de películas")
+    print("2. Mosterar las películas registradas")
+    print("3. Buscar película por genero")
+    print("4. Eliminar un registro por el título")
+    print("5. Ver estadísticas de las peliculas")
+    print("6. Salir del programa")
+
+    opcion=input("\nSeleccione la opcion que desee:")
+
+    match opcion:
+        case "1":
+            num_pls=int(input("\nIngrese la cantidad de peliculas de desea registrar: "))
+            agregar_pelicula(num_pls)
+            print("Pelicula agregada exitosamente")
+
+        case "2":
+            print("\nLas películas ingresadas son:")
+            mostrar_peliculas()
+
+
